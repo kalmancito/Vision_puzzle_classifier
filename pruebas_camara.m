1e475bc76a7acf6779883ff03ca959b5e234350f
Miguel-MSI\Miguel <angel.glopez@alumnos.upm.es>
2017-07-03 11:12:03 +0200

    segmentado lo de abajo con hsv y color.

--- pruebas_camara.m
+++ pruebas_camara.m
@@ -15,7 +15,7 @@ clc
 %%
 %    img = snapshot(cam);
 % img=imread('C:\Users\Miguel\Desktop\MUAR\1_sem\vision\vision\avion\aviona1.png ');
-img=imread('C:\Users\Miguel\Desktop\MUAR\1_sem\vision\vision\testm.jpg ');
+img=imread('C:\Users\Miguel\Desktop\MUAR\1_sem\vision\vision\testm6.jpg ');
 % img=imread('C:\Users\Miguel\Desktop\MUAR\1_sem\vision\vision\puzzle_vision_prueba.png ');
 
 % subplot(1,3,1);imshow(img(:,:,1))
@@ -62,29 +62,36 @@ figure
 imshow(label2rgb(LL));
 %%
 
-caract=regionprops(L,'all');
+% caract=regionprops(L,'all');
 % imshow(L==3)
 % caract(3).Area
 % caract(3).BoundingBox
 % caract(3).ConvexHull
-centroids = cat(1, caract.Centroid);
+
 
 %%
 j=0;
 STATS = regionprops(L,'all');
 for ii=1:length(STATS)
 % STATS = regionprops(L, 'Image', 'SubarrayIdx');
-        if (abs(STATS(ii).ConvexArea-(STATS(ii).Perimeter/4)^2)>7500)&&...
+        if (abs(STATS(ii).Area-(STATS(ii).Perimeter/4)^2)>1000)&&...
                 STATS(ii).Area>100000
            %abs(STATS(ii).EquivDiameter-(STATS(ii).Perimeter/2))<26
         j=j+1;
         caract(j)=STATS(ii);
         imMask = STATS(ii).Image;
         subImage{j} = L(STATS(ii).SubarrayIdx{:});
-        figure; imshow(subImage{j})
+%         figure; imshow(subImage{j})
         end
 end
-num=j;
+num=j
+clear centroids
+centroids = cat(1, caract.Centroid);
+%%
+imshow(bw)
+hold on
+plot(centroids(:,1),centroids(:,2), 'b*')
+hold off
 %% GIRAR EL CUADRADO
 close all
 for kk=1:num
@@ -107,7 +114,7 @@ ROI_c_r{kk}=imrotate(ROI_c{kk},0,'bilinear');
 ROI_bordes{kk}=edge(ROI_c_r{kk},'sobel');
 
 imshow(ROI_c_r{kk})
-pause
+% pause
 end
 
 
@@ -131,30 +138,41 @@ end
 
 %%
 
-imshow(bw)
-hold on
-plot(centroids(:,1),centroids(:,2), 'b*')
-hold off
 %%
 % figure(555)
 % img=rgb2hsv(img);
+close all
+ clear II II2 II3
+ j=0;
 for i=1:num
 % ROI_cH{i}=imcrop(img,[caract(i).ConvexHull]);
 
 % mask=poly2mask(,147,168)
 % ROI_c{i}=imcrop(img,[caract(i).BoundingBox]);
 % ROI_mask{i}=caract(i).ConvexImage;
-II=imcrop(rgb2gray(img),[caract(i).BoundingBox]);
-corners{i}=detectSURFFeatures(II,'MetricThreshold',500);
+II=imcrop(rgb2hsv(img),[caract(i).BoundingBox]);
+% corners{i}=detectSURFFeatures(II,'MetricThreshold',500);
+II2=2*II(:,:,1)-II(:,:,2)-II(:,:,3);
 
-imshow(II); hold on;
- plot(corners{i}.selectStrongest(10));
+II3=imbinarize(II2,0.6);
+figure; imshow(II)
+Ic(i)=sum(sum(II3))/caract(i).Area;
+title(num2str(Ic(i)))
+
+if(Ic(i))>0.1
+    j=j+1;
+% title(num2str(Ic(i)))
+title(['detectada: ' num2str(Ic(i))] )
+kk2{j}=II;
+% figure; imshow(II)
+end
+%  plot(corners{i}.selectStrongest(10));
 
 
 % II=rgb2hsv(II);
 M=caract(i).ConvexImage;
 % Inew = II(2:end,2:end,:).*double(repmat(M,[1,1,3])); %para HSV
-Inew = II(2:end,2:end,:).*uint8(repmat(M,[1,1,3]));
+% Inew = II(2:end,2:end,:).*uint8(repmat(M,[1,1,3]));
 
 % figure(i*100);imshow(Inew)
 ROI_c{i}=Inew;
@@ -174,11 +192,11 @@ ROI_c_r{i}=imrotate(ROI_c{i},0,'bilinear');
 % caract(i).Area
 % figure
 % subplot(2,round(num/2),i)
-figure;imshow(ROI_c_r{i})
-title(num2str(i))
+% figure;imshow(ROI_c_r{i})
+% title(num2str(i))
 end
-
-
+j
+stop
 %%
 % 
 % for ii=2:num
@@ -211,53 +229,45 @@ end
 
 %%
 
-ROI_c=ROI_c_r;
+% ROI_c=ROI_c_r;
 % save('masks','ROI_c')
-load 
+
 %%
 
 load binarymasks
-
-BWs2=BWs;
-for nt=1:num
-     
-    numrows=125;
-    numcols=125;
-    I=rgb2gray(ROI_c_r{nt});
-    DD = imresize(I,[numrows numcols], 'bilinear') ;
-    % figure,imshow(DD)
-    BWs{nt} = edge(DD,'canny');
-    figure, imshow(BWs{nt}), title('binary gradient mask');
-end
-% save('binarymasks','BWs')
+load masks % mascaras en ROI_c
+% originales de la foto en ROI_c_r
 %%
 
 kk=1;
-for mascara=1:num
+for mascara=1
     for prueba=1:num
  
-        masc=BWs2{mascara};      
+        masc=rgb2gray(ROI_c{mascara});      
 [numrows numcols]=size(masc);
-DD = imresize(BWs{prueba},[numrows numcols], 'bilinear') ;
+DD = imresize(rgb2gray(ROI_c_r{prueba}),[numrows numcols], 'bilinear') ;
 
 % figure;imshow(BWs{prueba})
-H{prueba}=DD|BWs2{mascara};
+% H{prueba}=conv2(single(DD),single(masc));
+H{prueba}=edge(masc-DD,'sobel')&(edge(masc,'sobel'));
+figure;imshow(edge(masc-DD,'sobel')&(edge(masc,'sobel')))
 % H=imfilter(DD,BWs2{mascara},'conv')
 
 % figure;imshow(H{prueba})
-res(mascara,prueba)=sum(sum(H{prueba}))/sum(sum(BWs2{mascara}));
-res2(mascara)=sum(sum(BWs2{mascara}));
+res(mascara,prueba)=sum(sum(H{prueba}))/sum(sum(edge(masc,'sobel')));
+% res(mascara,prueba)=max(H{prueba});
+% res2(mascara)=sum(sum(masc));
 
     end
 end
  close all
-res=1./res;
-size(res)
-
+% res=1./res;
+% size(res)
+% res
 
 % imshow(H{1})
 
-mesh(res)
+% mesh(res)
 stop
 
 
